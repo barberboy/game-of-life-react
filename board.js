285feecee3a07cf7ded5250d43938cc80d2e0fc2
crodeheaver <colbyrodeheaver@hotmail.com>
2015-09-11 20:49:53 +0000

    working draft basic game set up. needs some seeds and style.

---
+++ board.js
@@ -0,0 +1,85 @@
+var CellStates = {
+    DEAD: " dead",
+    ALIVE: " alive"
+};
+var Board = function(height, width, size) {
+    this.width = width;
+    this.height = height;
+    this.size = size;
+    this.board = this.create_board(width, height);
+
+};
+
+Board.prototype.create_board = function(height, width) {
+    var board = [];
+    for (var y = 0; y < width; y++) {
+        board[y] = [];
+        for (var x = 0; x < height; x++)
+            board[y][x] = CellStates.DEAD;
+    }
+    return board;
+};
+
+Board.prototype.changeState = function(y, x) {
+    console.log("Played at " + y + ", " + x);
+    if (this.board[y][x] == CellStates.DEAD)
+        this.board[y][x] = CellStates.ALIVE;
+    else
+        this.board[y][x] = CellStates.DEAD;
+
+    return true;
+};
+
+Board.prototype.cycle = function() {
+    var LiveOrDie = [];
+    for (var y = 0; y < this.height; y++) {
+        for (var x = 0; x < this.width; x++) {
+            
+                    var thisPosX = x;
+                    var thisPosY = y;
+                    var MIN_X = 0;
+                    var MIN_Y = 0;
+                    var MAX_X = this.width-1;
+                    var MAX_Y = this.height-1;
+
+                    var startPosX = (thisPosX - 1 < MIN_X) ? thisPosX : thisPosX - 1;
+                    var startPosY = (thisPosY - 1 < MIN_Y) ? thisPosY : thisPosY - 1;
+                    var endPosX = (thisPosX + 1 > MAX_X) ? thisPosX : thisPosX + 1;
+                    var endPosY = (thisPosY + 1 > MAX_Y) ? thisPosY : thisPosY + 1;
+                    
+                    var livingNeighbors = 0;
+                    
+                    for (var rowNum = startPosY; rowNum <= endPosY; rowNum++) {
+                        for (var colNum = startPosX; colNum <= endPosX; colNum++) {
+                            if(this.board[rowNum][colNum] == CellStates.ALIVE && (rowNum != y || colNum !=x))
+                                livingNeighbors++;
+                        }
+                    }
+                    switch (this.board[y][x]) {
+                case CellStates.DEAD:
+                    if(livingNeighbors == 3)
+                    LiveOrDie.push({X:x,Y:y});
+                        //this.board[y][x] = CellStates.ALIVE;
+                    break;
+                case CellStates.ALIVE:
+                    if(livingNeighbors < 2 || livingNeighbors > 3)
+                        LiveOrDie.push({X:x,Y:y});
+                    break;
+                default:
+                    // code
+                    break;
+            }
+        }
+    }
+    console.log(LiveOrDie);
+    for(var i = 0; i < LiveOrDie.length; i++)
+    {
+        //this.changeState(LiveOrDie[i].Y,LiveOrDie[i].X);
+        if (this.board[LiveOrDie[i].Y][LiveOrDie[i].X] == CellStates.DEAD)
+            this.board[LiveOrDie[i].Y][LiveOrDie[i].X] = CellStates.ALIVE;
+        else
+            this.board[LiveOrDie[i].Y][LiveOrDie[i].X] = CellStates.DEAD;
+        console.log(LiveOrDie[i].Y+ " " +LiveOrDie[i].X)
+    }
+    return true;
+}
---
+++ game_of_life.js
@@ -0,0 +1,112 @@
+/** @jsx React.DOM */
+var GRID_SIZE = 10;
+var BOARD_WIDTH = 60;
+var BOARD_HEIGHT = 30;
+
+var PlayButton = React.createClass({
+    text: "Play",
+    interval:null,
+    run: function() {
+        this.props.board.cycle();
+        this.props.onPlay();
+    },
+    handleClick: function() {
+        if (this.text == "Play") {
+            interval = setInterval(this.run, 500);
+            this.text = "Stop";
+        }
+        else {
+            window.clearInterval(interval);
+            this.text = "Play";
+        }
+        this.props.onPlay();
+        
+
+    },
+    render: function() {
+        return (
+            <div class = "playbutton">
+             <button onClick={this.handleClick} type="button">{this.text}</button> 
+            </div>
+        )
+    }
+});
+
+var BoardCell = React.createClass({
+    handleClick: function() {
+        if (this.props.board.changeState(this.props.y, this.props.x))
+            this.props.onPlay();
+    },
+    render: function() {
+        var style = {
+            top: this.props.y * GRID_SIZE,
+            left: this.props.x * GRID_SIZE
+        };
+
+        var classes = "cell";
+        classes += this.props.cell;
+        return (
+            <div onClick={this.handleClick} 
+                 className={classes} style={style}></div>
+        );
+    }
+});
+
+var BoardView = React.createClass({
+    render: function() {
+        var cells = [];
+        for (var i = 0; i < this.props.board.height; i++)
+            for (var j = 0; j < this.props.board.width; j++) {
+                cells.push(BoardCell({
+                    board: this.props.board,
+                    cell: this.props.board.board[i][j],
+                    y: i,
+                    x: j,
+                    onPlay: this.props.onPlay
+                }));
+
+            }
+
+        var style = {
+            width: this.props.board.width * GRID_SIZE,
+            height: this.props.board.height * GRID_SIZE
+        };
+        return <div style={style} id="board">{cells}</div>;
+    }
+});
+
+
+var board = new Board(BOARD_HEIGHT, BOARD_WIDTH, GRID_SIZE);
+
+
+var ContainerView = React.createClass({
+    getInitialState: function() {
+        return {
+            'board': this.props.board
+        };
+    },
+    onBoardUpdate: function() {
+        this.setState({
+            "board": this.props.board
+        });
+    },
+    render: function() {
+
+        return (
+            <div>
+                <PlayButton board={this.state.board}
+                onPlay={this.onBoardUpdate.bind(this)}/>
+                <BoardView board={this.state.board} 
+                    onPlay={this.onBoardUpdate.bind(this)} />
+                
+            </div>
+        )
+    }
+});
+
+
+
+React.renderComponent(
+    <ContainerView board={board} />,
+    document.getElementById('main')
+);
