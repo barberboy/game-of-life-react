aaa7a72fa42107a86091332631fa0ce07f131587
crodeheaver <colbyrodeheaver@hotmail.com>
2015-09-16 15:37:43 +0000

    Modified setup changed game_of_life.js to game_of_life.jsx and used babel to convert it to regular javascript Modified index.html to use the pure javascript game of life instead of using jsxtransformer to do it on the fly.

--- game_of_life.js
+++ game_of_life.js
@@ -1,97 +1,125 @@
-
 /* global React */
 /* global Board */
+"use strict";
+
 var GRID_SIZE = 10;
 var BOARD_WIDTH = 60;
 var BOARD_HEIGHT = 30;
 
 var PlayButton = React.createClass({
+    displayName: "PlayButton",
+
     text: "Play",
-    interval:null,
-    run: function() {
+    interval: null,
+    run: function run() {
         this.props.board.cycle();
         this.props.onPlay();
     },
-    handleClick: function() {
+    handleClick: function handleClick() {
         if (this.text == "Play") {
             this.interval = setInterval(this.run, 500);
             this.text = "Stop";
-        }
-        else {
+        } else {
             window.clearInterval(this.interval);
             this.text = "Play";
         }
         this.props.onPlay();
     },
-    render: function() {
-        return (
-            <div className="playbutton">
-             <button onClick={this.handleClick} type="button">{this.text}</button> 
-            </div>
-        )
+    render: function render() {
+        return React.createElement(
+            "div",
+            { className: "playbutton" },
+            React.createElement(
+                "button",
+                { onClick: this.handleClick, type: "button" },
+                this.text
+            )
+        );
     }
 });
 
 var ClearButton = React.createClass({
+    displayName: "ClearButton",
+
     text: "Clear",
-    handleClick: function() {
+    handleClick: function handleClick() {
         this.props.board.clear_board();
         this.props.onPlay();
     },
-    render: function() {
-        return (
-            <div className="clearbutton">
-             <button onClick={this.handleClick} type="button">{this.text}</button> 
-            </div>
-        )
+    render: function render() {
+        return React.createElement(
+            "div",
+            { className: "clearbutton" },
+            React.createElement(
+                "button",
+                { onClick: this.handleClick, type: "button" },
+                this.text
+            )
+        );
     }
 });
 
 var RandomSeedButton = React.createClass({
+    displayName: "RandomSeedButton",
+
     text: "Random Seed",
-    handleClick: function() {
+    handleClick: function handleClick() {
         this.props.board.random_seed();
         this.props.onPlay();
     },
-    render: function() {
-        return (
-            <div className="randomseedbutton">
-             <button onClick={this.handleClick} type="button">{this.text}</button> 
-            </div>
-        )
+    render: function render() {
+        return React.createElement(
+            "div",
+            { className: "randomseedbutton" },
+            React.createElement(
+                "button",
+                { onClick: this.handleClick, type: "button" },
+                this.text
+            )
+        );
     }
 });
 
 var Seed = React.createClass({
-    handleClick: function(){
+    displayName: "Seed",
+
+    handleClick: function handleClick() {
+        console.log(this.props.item);
         this.props.board.clear_board();
         this.props.board.seed(this.props.item);
         this.props.onPlay();
     },
-    render: function(){
-        return(<option onClick={this.handleClick}>{this.props.item}</option>)
+    render: function render() {
+        return React.createElement(
+            "option",
+            { onClick: this.handleClick },
+            this.props.item
+        );
     }
 });
 
 var SeedSelect = React.createClass({
-    render: function() {
-        return (
-            <select>
-                <Seed item={"Glider"} board={this.props.board} onPlay={this.props.onPlay} />
-                <Seed item={"Pulsar"} board={this.props.board} onPlay={this.props.onPlay} /> 
-                <Seed item={"Pentadecathlon"} board={this.props.board} onPlay={this.props.onPlay} /> 
-                <Seed item={"Lightweight spaceship"} board={this.props.board} onPlay={this.props.onPlay} /> 
-            </select>
-        )
+    displayName: "SeedSelect",
+
+    render: function render() {
+        return React.createElement(
+            "select",
+            null,
+            React.createElement(Seed, { item: "Glider", board: this.props.board, onPlay: this.props.onPlay }),
+            React.createElement(Seed, { item: "Pulsar", board: this.props.board, onPlay: this.props.onPlay }),
+            React.createElement(Seed, { item: "Pentadecathlon", board: this.props.board, onPlay: this.props.onPlay }),
+            React.createElement(Seed, { item: "Lightweight spaceship", board: this.props.board, onPlay: this.props.onPlay })
+        );
     }
 });
 
 var BoardCell = React.createClass({
-    handleClick: function() {
-        if (this.props.board.changeState(this.props.y, this.props.x))
-            this.props.onPlay();
+    displayName: "BoardCell",
+
+    handleClick: function handleClick() {
+        if (this.props.board.changeState(this.props.y, this.props.x)) this.props.onPlay();
     },
-    render: function() {
+    render: function render() {
         var style = {
             top: this.props.y * GRID_SIZE,
             left: this.props.x * GRID_SIZE
@@ -99,65 +127,64 @@ var BoardCell = React.createClass({
 
         var classes = "cell";
         classes += this.props.cell;
-        return (
-            <div onClick={this.handleClick} className={classes} style={style}></div>
-        );
+        return React.createElement("div", { onClick: this.handleClick, className: classes, style: style });
     }
 });
 
 var BoardView = React.createClass({
-    render: function() {
-        
+    displayName: "BoardView",
+
+    render: function render() {
+
         var cells = [];
-        for (var i = 0; i < this.props.board.height; i++)
-            for (var j = 0; j < this.props.board.width; j++) {
-                cells.push(<BoardCell board={this.props.board}
-                                      cell={this.props.board.board[i][j]} 
-                                      y={i} 
-                                      x={j} 
-                                      onPlay={this.props.onPlay}
-                                      key={j+""+i+j}/>)
-            }
-            
+        for (var i = 0; i < this.props.board.height; i++) for (var j = 0; j < this.props.board.width; j++) {
+            cells.push(React.createElement(BoardCell, { board: this.props.board,
+                cell: this.props.board.board[i][j],
+                y: i,
+                x: j,
+                onPlay: this.props.onPlay,
+                key: j + "" + i + j }));
+        }
+
         var style = {
             width: this.props.board.width * GRID_SIZE,
             height: this.props.board.height * GRID_SIZE
         };
-        
-        return <div style={style} id="board" >{cells}</div>;
+
+        return React.createElement(
+            "div",
+            { style: style, id: "board" },
+            cells
+        );
     }
 });
 
 var board = new Board(BOARD_HEIGHT, BOARD_WIDTH, GRID_SIZE);
 
-
 var ContainerView = React.createClass({
-    getInitialState: function() {
+    displayName: "ContainerView",
+
+    getInitialState: function getInitialState() {
         return {
             'board': this.props.board
         };
     },
-    onBoardUpdate: function() {
+    onBoardUpdate: function onBoardUpdate() {
         this.setState({
             "board": this.props.board
         });
     },
-    render: function() {
-        return (
-            <div>
-                <PlayButton board={this.state.board} onPlay={this.onBoardUpdate}/>
-                <ClearButton board={this.state.board} onPlay={this.onBoardUpdate}/>
-                <RandomSeedButton board={this.state.board} onPlay={this.onBoardUpdate}/>
-                <SeedSelect board={this.state.board} onPlay={this.onBoardUpdate} />
-                <BoardView board={this.state.board} onPlay={this.onBoardUpdate} />
-            </div>
+    render: function render() {
+        return React.createElement(
+            "div",
+            null,
+            React.createElement(PlayButton, { board: this.state.board, onPlay: this.onBoardUpdate }),
+            React.createElement(ClearButton, { board: this.state.board, onPlay: this.onBoardUpdate }),
+            React.createElement(RandomSeedButton, { board: this.state.board, onPlay: this.onBoardUpdate }),
+            React.createElement(SeedSelect, { board: this.state.board, onPlay: this.onBoardUpdate }),
+            React.createElement(BoardView, { board: this.state.board, onPlay: this.onBoardUpdate })
         );
     }
 });
 
-
-
-React.render(
-    <ContainerView board={board} />,
-    document.getElementById('main')
-);
+React.render(React.createElement(ContainerView, { board: board }), document.getElementById('main'));
---
+++ jsx/game_of_life.jsx
@@ -0,0 +1,163 @@
+/* global React */
+/* global Board */
+var GRID_SIZE = 10;
+var BOARD_WIDTH = 60;
+var BOARD_HEIGHT = 30;
+
+var PlayButton = React.createClass({
+    text: "Play",
+    interval: null,
+    run: function() {
+        this.props.board.cycle();
+        this.props.onPlay();
+    },
+    handleClick: function() {
+        if (this.text == "Play") {
+            this.interval = setInterval(this.run, 500);
+            this.text = "Stop";
+        }
+        else {
+            window.clearInterval(this.interval);
+            this.text = "Play";
+        }
+        this.props.onPlay();
+    },
+    render: function() {
+        return (
+            <div className="playbutton">
+             <button onClick={this.handleClick} type="button">{this.text}</button> 
+            </div>
+        );
+    }
+});
+
+var ClearButton = React.createClass({
+    text: "Clear",
+    handleClick: function() {
+        this.props.board.clear_board();
+        this.props.onPlay();
+    },
+    render: function() {
+        return (
+            <div className="clearbutton">
+             <button onClick={this.handleClick} type="button">{this.text}</button> 
+            </div>
+        )
+    }
+});
+
+var RandomSeedButton = React.createClass({
+    text: "Random Seed",
+    handleClick: function() {
+        this.props.board.random_seed();
+        this.props.onPlay();
+    },
+    render: function() {
+        return (
+            <div className="randomseedbutton">
+             <button onClick={this.handleClick} type="button">{this.text}</button> 
+            </div>
+        );
+    }
+});
+
+var Seed = React.createClass({
+    handleClick: function() {
+        console.log(this.props.item);
+        this.props.board.clear_board();
+        this.props.board.seed(this.props.item);
+        this.props.onPlay();
+    },
+    render: function() {
+        return (<option onClick={this.handleClick}>{this.props.item}</option>);
+    }
+});
+
+var SeedSelect = React.createClass({
+    render: function() {
+        return (
+            <select>
+                <Seed item={"Glider"} board={this.props.board} onPlay={this.props.onPlay} />
+                <Seed item={"Pulsar"} board={this.props.board} onPlay={this.props.onPlay} /> 
+                <Seed item={"Pentadecathlon"} board={this.props.board} onPlay={this.props.onPlay} /> 
+                <Seed item={"Lightweight spaceship"} board={this.props.board} onPlay={this.props.onPlay} /> 
+            </select>
+        );
+    }
+});
+
+var BoardCell = React.createClass({
+    handleClick: function() {
+        if (this.props.board.changeState(this.props.y, this.props.x))
+            this.props.onPlay();
+    },
+    render: function() {
+        var style = {
+            top: this.props.y * GRID_SIZE,
+            left: this.props.x * GRID_SIZE
+        };
+
+        var classes = "cell";
+        classes += this.props.cell;
+        return (
+            <div onClick={this.handleClick} className={classes} style={style}></div>
+        );
+    }
+});
+
+var BoardView = React.createClass({
+    render: function() {
+
+        var cells = [];
+        for (var i = 0; i < this.props.board.height; i++)
+            for (var j = 0; j < this.props.board.width; j++) {
+                cells.push(<BoardCell board={this.props.board}
+                                      cell={this.props.board.board[i][j]} 
+                                      y={i} 
+                                      x={j} 
+                                      onPlay={this.props.onPlay}
+                                      key={j+""+i+j}/>);
+            }
+
+        var style = {
+            width: this.props.board.width * GRID_SIZE,
+            height: this.props.board.height * GRID_SIZE
+        };
+
+        return <div style={style} id="board" >{cells}</div>;
+    }
+});
+
+var board = new Board(BOARD_HEIGHT, BOARD_WIDTH, GRID_SIZE);
+
+
+var ContainerView = React.createClass({
+    getInitialState: function() {
+        return {
+            'board': this.props.board
+        };
+    },
+    onBoardUpdate: function() {
+        this.setState({
+            "board": this.props.board
+        });
+    },
+    render: function() {
+        return (
+            <div>
+                <PlayButton board={this.state.board} onPlay={this.onBoardUpdate}/>
+                <ClearButton board={this.state.board} onPlay={this.onBoardUpdate}/>
+                <RandomSeedButton board={this.state.board} onPlay={this.onBoardUpdate}/>
+                <SeedSelect board={this.state.board} onPlay={this.onBoardUpdate} />
+                <BoardView board={this.state.board} onPlay={this.onBoardUpdate} />
+            </div>
+        );
+    }
+});
+
+
+
+React.render(
+    <ContainerView board={board} />,
+    document.getElementById('main')
+);
